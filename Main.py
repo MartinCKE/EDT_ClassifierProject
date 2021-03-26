19d6b1bd48123d5cad2b4ce9ad4a5a3817b4711c
MartinCKE <blackopsck@gmail.com>
2021-03-26 08:56:36 +0100

    Opening iris data

--- Main.py
+++ Main.py
@@ -3,3 +3,16 @@
 import math
 import numpy
 import matplotlib as plt
+import os
+
+
+__location__ = os.path.realpath(
+    os.path.join(os.getcwd(), os.path.dirname(__file__)))
+
+irisDataLoc = os.path.join(__location__, 'Data/Iris_TTT4275/iris.data')
+
+with open(irisDataLoc) as file: ##
+    for line in file:
+        print(line)
+        for word in line:
+            print(word)
